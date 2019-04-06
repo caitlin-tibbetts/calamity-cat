734c29fe8b896bd1ebc10dd5c211ced1b9c67c74
beaagron <baagron@gmail.com>
2019-04-06 18:16:39 -0500

    adding maps and text input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Input from './components/input';
+import cat from './cat.svg';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class App extends Component {
+
+  handle (event) 
+  {
+    this.setState({
+        data:event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={cat} className="App-logo" alt="logo" />
+          <h1>
+            welcome to calamity cat
+          </h1>
+          <input className="text-input" type="text" onChange={ this.handle.bind(this)}/>
         </header>
+
+        <Map google={this.props.google} zoom={14}>
+ 
+        <Marker className="map" onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+
+        </InfoWindow>
+      </Map>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyD0YGgL8ceGfhi-im1fDopabKgLuL99mls")
+})(App)
---
+++ my-app/src/components/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Input extends Component {
+
+    render() {
+        return (
+            <div className="field">
+            </div>
+        )
+    }
+}
+
+export default Input;
