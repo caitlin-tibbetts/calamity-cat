df5fced7f357c2d212db6617047f7a42e15f08ed
Caitlin Tibbetts <caitlin@tibbettsfamily.com>
2019-04-07 02:18:45 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Input from './Input';
 import cat from './cat.svg';
+import Geocode from "react-geocode";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+Geocode.setApiKey("AIzaSyAGSN1jKfDiCmSuJbO0EdAFrB3NUqdQTJM");
+
+var lat = 0;
+var lng = 0;
+
 class App extends Component {
-
-  // this is the function for the generate button
-
-  handleClick = () => {
-    console.log('this is:', this);
+  constructor(props) {
+    super (props);
+    this.state = {
+      lat: 0, lng: 0
+    }
   }
 
-  state = {
-    markers: [
-      {
-        name: "Current position",
-        position: {
-          lat: 37.77,
-          lng: -122.42
-        }
-      }
-    ]
+  updateCoordinates = (lat, lng) => {
+    this.state.lat = lat;
+    this.state.lng = lng;
   };
-
-  onMarkerDragEnd = (one, two, three) => { 
-    const { latLng } = three; 
-    const lat = latLng.lat(); 
-    const lng = latLng.lng(); 
-    console.log(lat); };
 
   render() {
     return (
@@ -41,36 +33,62 @@ class App extends Component {
           <h2 className="App-subheader">
             avoid cat-tastrophe
           </h2>
-          <Input />
-          <button className="button" onClick={this.handleClick}>
-            <h1 className="button-text">GENERATE!</h1>
-          </button>
+          <AddressForm triggerParentUpdate={this.updateCoordinates}/>
         </header>
 
         <Map google={this.props.google} zoom={14}>
 
-          {this.state.markers.map((marker, index) => (
-            <Marker
-              position={marker.position}
-              draggable={true}
-              onDragEnd={this.onMarkerDragEnd}
-              name={marker.name}
-            />
-            
-          ))}
-          {/* <InfoWindow
-            onClose={this.onInfoWindowClose}
-          >
-            <div>
-              <p>Click on the map or drag the marker to select location where the incident occurred</p>
-            </div>
-          </InfoWindow> */}
+          <Marker className="map" onClick={this.onMarkerClick}
+            name={'Current location'} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+
+          </InfoWindow>
         </Map>
-      </div >
+      </div>
+    );
+  }
+}
+
+class AddressForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {address: '', lat: 0, lng: 0};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({address: event.target.value});
+  }
+
+  handleSubmit(event) {
+    Geocode.fromAddress(this.state.address).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        this.setState({lat: lat, lng: lng});
+        this.props.triggerParentUpdate(this.state.lat, this.state.lng);
+      },
+      error => {
+        console.error(error);
+      }
+    )
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.value} placeholder="Where is...?" onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Go!"/>
+      </form>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyD0YGgL8ceGfhi-im1fDopabKgLuL99mls")
+  apiKey: ("AIzaSyAGSN1jKfDiCmSuJbO0EdAFrB3NUqdQTJM")
 })(App)
